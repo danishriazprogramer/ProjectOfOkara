623af378ef32a3b3e5835dbe557c88485f5efb58
danishriazprogramer <88670280+danishriazprogramer@users.noreply.github.com>
2023-03-08 21:13:19 +0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,62 @@
+const express=require("express");
+
+const app=express();
+
+const port=3000;
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+const routes = require("../routes/main");
+const mongoose=require("mongoose")
+
+ const  dotenv=require("dotenv").config()
+
+
+app.use("", routes)
+app.use("/static",express.static("public"))
+app.set("view engine", "ejs");
+app.set("views","views/partials")
+
+
+const connectionParams={
+  useNewUrlParser: true,
+
+  useUnifiedTopology: true ,
+    
+      
+}
+
+mongoose.connect('mongodb+srv://danishriazprogramer:4mjpL2GyHwcWKwQf@cluster0.fzjs5cn.mongodb.net/?retryWrites=true&w=majority/nodejsapp',connectionParams)
+
+  .then(() => console.log('Connected!'));
+
+
+// session connection
+
+const  sessionConnection=mongoose.connection.once('open',()=>{
+    console.log("connetion mongo for session");
+
+})
+
+
+
+
+const session = require('express-session');
+const bodyParser = require("body-parser");
+const MongoStore = require('connect-mongo')(session);
+
+app.use(bodyParser.json)
+
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+
+
+
+
+
+   
+app.listen(port,()=>{
+console.log('server stated on port :>> ', port);
+
+});
